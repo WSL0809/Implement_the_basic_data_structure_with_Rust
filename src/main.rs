bd0d61036f6954b784b58a260c7c2eb4d1202aca
wsl <wslzzy@qq.com>
2023-01-20 11:30:26 +0800

    队列

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-fn add<T: std::ops::Add<Output = T>>(a:T, b:T) -> T {
-    a + b
-}
+mod queue;
 fn main() {
-    println!("add i8: {}", add(2i8, 3i8));
-    println!("add i32: {}", add(20, 30));
-    println!("add f64: {}", add(1.23, 1.23));
+
 }
---
+++ src/queue.rs
@@ -0,0 +1,64 @@
+#[derive(Debug)]
+struct Queue<T>{
+    capacity: usize,
+    data:Vec<T>,
+}
+
+impl<T> Queue<T> {
+    fn new(size: usize) -> Self {
+        Queue {
+            capacity: size,
+            data: Vec::with_capacity(size),
+        }
+    }
+
+    fn size (&self) -> usize {
+        self.data.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        Self::size(&self) == 0
+    }
+
+    fn enqueue(&mut self, val: T) -> Result<(),String> {
+        if Self::size(&self) == self.capacity{
+            return Err("No Space Available".to_string());
+        }
+        self.data.insert(0, val);
+        Ok(())
+    }
+
+    fn dequeue(&mut self) -> Option<T> {
+        if Self::size(&self) > 0{
+            self.data.pop()
+        }else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works(){
+        let mut q = Queue::new(3);
+        let r1 = q.enqueue(1);
+        let r2 = q.enqueue(2);
+        let r3 = q.enqueue(3);
+
+        if let Err(e) = q.enqueue(4){
+            println!("{}", e);
+        }
+
+        if let Some (data) = q.dequeue () {
+            println!("data: {data}");
+        }else {
+            println!("empty queue.");
+        }
+
+        println!("size:{}, empty:{}", q.size(), q.is_empty ());
+        println!("content: {:?}", q) ;
+    }
+}
